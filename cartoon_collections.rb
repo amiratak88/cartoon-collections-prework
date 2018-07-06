c4798011d02116014414dbe073e36a3b6339cd81
Amirata K <amiratak88@gmail.com>
2018-07-06 05:23:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
-  arr.map{|captain| "#{captain}!"}
+  arr.map{|captain| "#{captain.capitalize}!"}
 end
 
 def long_planeteer_calls(arr)
